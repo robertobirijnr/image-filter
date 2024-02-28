cd2a6728930ef2c0a22af9ce9ea3bb74b7fae97d
Robert Obiri Jr <robert.obiri@curogens.com>
2024-02-28 23:12:19 +0000

    store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="font-['Quicksand'] max-w-xl bg-white p-8 shadow-2xl rounded absolute m-auto left-0 right-0 mt-32">
-      <h1 class="text-center text-3xl text-indigo-700">Vue Filters</h1>
+      <h1 class="text-center text-3xl text-indigo-700">Image Filters</h1>
       <div class="text-center my-8 py-32 text-slate-700 italic rounded border-4 border-double border-slate-700/25">
         Drag and drop image.
       </div>
@@ -17,4 +17,5 @@
         </div>
       </div>
    </div>
+   <AppBG/>
 </template>
